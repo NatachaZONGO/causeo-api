a8789031869f6eafc13e67e98ef8ec6340436665
NatachaZONGO <natachzong@gmail.com>
2026-09-09 02:48:15 +0000

    Fix: use created_at for latestOfMany instead of UUID

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -71,6 +71,6 @@ class Conversation extends Model
      */
     public function latestMessage(): HasOne
     {
-        return $this->hasOne(Message::class)->latestOfMany();
+        return $this->hasOne(Message::class)->latestOfMany('created_at');
     }
 }
