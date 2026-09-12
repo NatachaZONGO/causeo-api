729e7af35e7a72fd2b9ad146b18aceb29526bdcd
NatachaZONGO <natachzong@gmail.com>
2026-09-12 01:22:36 +0000

    Run migrations and seed templates on deploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,7 @@ set -e
 # Les variables d'environnement Render sont disponibles ici (runtime).
 php artisan config:cache
 php artisan route:cache
+php artisan migrate --force
+php artisan db:seed --class=BusinessTemplateSeeder --force
 
 exec "$@"
