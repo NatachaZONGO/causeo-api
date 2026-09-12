9cc93deaa964a615d0b0004a10f2333c17ad408b
NatachaZONGO <natachzong@gmail.com>
2026-09-12 14:55:02 +0000

    Fix: remove redirect_uri, add grant_type for token exchange

--- app/Http/Controllers/API/V1/WhatsAppSetupController.php
+++ app/Http/Controllers/API/V1/WhatsAppSetupController.php
@@ -120,7 +120,7 @@ class WhatsAppSetupController extends Controller
                     'client_id' => config('services.facebook.app_id'),
                     'client_secret' => config('services.facebook.app_secret'),
                     'code' => $code,
-                    'redirect_uri' => '',
+                    'grant_type' => 'authorization_code',
                 ],
             ]);
 
